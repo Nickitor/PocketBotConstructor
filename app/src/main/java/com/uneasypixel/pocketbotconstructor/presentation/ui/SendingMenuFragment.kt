7d710f69cf2d161052be8b0428c5d0b1742c0404
Nikita <38568177+Nickitor@users.noreply.github.com>
2021-05-23 23:39:32 +0300

    Исправлено меню рассылки сообщений

--- app/src/main/java/com/uneasypixel/pocketbotconstructor/presentation/ui/SendingMenuFragment.kt
+++ app/src/main/java/com/uneasypixel/pocketbotconstructor/presentation/ui/SendingMenuFragment.kt
@@ -5,17 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.uneasypixel.pocketbotconstructor.ProgApplication
+import com.uneasypixel.pocketbotconstructor.R
 import com.uneasypixel.pocketbotconstructor.databinding.FragmentSendingMenuBinding
+import com.uneasypixel.pocketbotconstructor.domain.entities.Bot
+import com.uneasypixel.pocketbotconstructor.presentation.viewmodels.ListOfBotsViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class SendingMenuFragment : Fragment() {
 
     // Объект привязки для получения объектов интерфейса
-    private var _binding: FragmentSendingMenuBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentSendingMenuBinding
+
+    private val listOfBotsViewModel: ListOfBotsViewModel by activityViewModels()
+    var bot: Bot? = null
+
+    // Создание фрагмента
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setBotToViewModel(arguments?.getString("BOT_NAME_KEY"))
+    }
 
 
     // Создание макета фрагмента
@@ -23,8 +37,9 @@ class SendingMenuFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSendingMenuBinding.inflate(inflater, container, false)
+        binding = FragmentSendingMenuBinding.inflate(inflater, container, false)
 
+        setOnBackPressedCallback()
         return binding.root
     }
 
@@ -33,24 +48,45 @@ class SendingMenuFragment : Fragment() {
 
         binding.sendingMenuButtonSend.setOnClickListener {
 
-            val tokenGroup = "88a1d21f807fe5a534ebd62721612411fe5e2fcdd6d15a65fb879b84754f91d60d25f68b0cc116b9c3f78"
-
             GlobalScope.launch {
 
                 val sendMessageToConversations = (requireActivity().application as ProgApplication).dependencyFactory.provideSendMessageToConversationsUseCase()
 
                 sendMessageToConversations.sendMessageToConversations(
                     binding.sendingMenuSendingTextEditTextTextMultiLine.text.toString(),
-                    tokenGroup)
+                    bot!!.token)
             }
 
             Toast.makeText(requireContext(), "Сообщения отправлены пользователям!", Toast.LENGTH_SHORT).show()
         }
     }
 
+
+    private fun setOnBackPressedCallback() {
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    findNavController().navigate(R.id.action_sendingMenuFragment_to_botMenuFragment)
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
+
+
+    fun setBotToViewModel(botName : String?) {
+        if (botName != null) {
+            for (bot in listOfBotsViewModel.listOfBots) {
+                if (bot.name == botName) {
+                    this.bot = bot
+                    return
+                }
+            }
+        }
+    }
+
+
     // Удаление компнентов внутри фрагмента
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 }
